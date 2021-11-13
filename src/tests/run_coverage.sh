d2741968cd6b5feb1d4f65f435c0702afcc8a9a3
Steven Mueller <steven.mueller@gmail.com>
2021-11-13 12:03:58 -0700

    taketake: add coverage script for tests
    run_coverage.sh runs tests under coverage run --branch, generates an html
    report, then launches the report in a browser using xdg-open.

---
+++ src/tests/run_coverage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Runs conditional (branch) coverage, generates an html report, opens the
+# report in a webpage
+
+if which coverage > /dev/null; then
+
+    echo "NOTE: coverage doesn't work on NFS mounts - it hangs due to lack of lock support!"
+    # strace reports:
+    # fcntl(3, F_SETLK, {l_type=F_RDLCK, l_whence=SEEK_SET, l_start=1073741824, l_len=1}) = -1 ENOLCK (No locks available)
+    set -e -x
+    time TEST_TAKETAKE_DONTSKIP=1 coverage run --branch -m unittest discover
+    coverage html
+    xdg-open htmlcov/index.html
+
+else
+    echo "No coverage tool found.  Install Python coverage with:"
+    echo "   python3 -m pip install --user coverage"
+    exit 2
