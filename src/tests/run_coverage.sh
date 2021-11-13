7654ab1193a3011c10b543cae80b23cf416e93f1
Steven Mueller <steven.mueller@gmail.com>
2021-11-13 12:17:56 -0700

    taketake: fix syntax error in run_coverage.sh

--- src/tests/run_coverage.sh
+++ src/tests/run_coverage.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Runs conditional (branch) coverage, generates an html report, opens the
-# report in a webpage
+# Runs conditional (branch) coverage, generates an html report, and opens the
+# report in a web browser
 
 if which coverage > /dev/null; then
 
-    echo "NOTE: coverage doesn't work on NFS mounts - it hangs due to lack of lock support!"
+    echo "NOTE: coverage doesn't work on some NFS mounts - it hangs due to lack of lock support!"
+    echo
     # strace reports:
     # fcntl(3, F_SETLK, {l_type=F_RDLCK, l_whence=SEEK_SET, l_start=1073741824, l_len=1}) = -1 ENOLCK (No locks available)
     set -e -x
@@ -17,3 +18,4 @@ else
     echo "No coverage tool found.  Install Python coverage with:"
     echo "   python3 -m pip install --user coverage"
     exit 2
+fi
